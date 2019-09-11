c0e19067cfea8163311dcbba378552f488aa205b
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-09-11 11:42:54 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+/// IF THEN STATEMENTS
+/* let firstName = 'Paul';
+let status = 'singe';
+
+if ( status == 'single') {
+    console.log ('${firstName} is single and ready to mingle.');
+
+} else{
+    console.log ('Someone made a big mistake with ${firstName}');
+
+} */
+
+/* let firstName = 'Sarah';
+let age = 17;
+
+if (age < 13){
+    console.log (`${firstName} is a young girl`);
+} else if (age >= 13 && age < 20){
+    console.log(`${firstName} is a young women`);
+} else {
+    console.log('She a boomer');
+} */
+////TERNARY opertor
+/* let firstName = 'Melkadze';
+let age = 17;
+
+age >= 21 ? console.log(`${firstName} will drink alcohol`): console.log (`${firstName} will drink choco milk`);
+ */
+///condition ? expIfTrue : eprIfFalse 
+
+//// Turthy and Falsey and equality operations 
+
+/// falsey values : null, 0 , undefined , '', NaN
+/// Truthy values : anything not above this line, anything 1 and above
+
+/* let height = 22;
+
+if (height == '22'){
+    console.log ('Hey this is type coersion!');
+} else {
+    console.log('No type coersion with triple ===');
+} */
+
+
+
