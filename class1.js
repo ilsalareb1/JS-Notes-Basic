2461503f022ef0c5cdaed3c13eb98f299c8f1c2c
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-09-12 11:32:17 -0400

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,39 @@
+/* //////Primivites//
+let numbers = 4 ;
+let numbers = 5 ;
+let numbersArrays = [1,2,3,4,5];
+console.log(numbers); 
+
+let string = 'string' ;
+let strings = 'strings' ;
+
+let boolan = true ; //also false 
+
+
+ */
+
+ /* const firstName = 'Rob' ;
+ const job = 'Student' ;
+ const birthYear = '2002' ;
+ const isMarried = false ;
+
+ console.log('Hello my name is ${firstName}, I am a ${job}, I was born in ${birthYear}' ); */
+
+ const now = new Date(). getFullYear();
+ 
+
+ const yearIlsa = 2003;
+let fullAge = 18;
+let isFullAge = now - yearIlsa >= fullAge;
+/* console.log (isFullAge); */
+let ageWill = now - 2002 ;
+let ageNick = 18;
+let averageAge= ( ageWill + ageNick)/2;
+/* console.log(averageAge); */
+
+let x,y;
+x = 5;
+y = x;
+z = y;
+q = z;
+console.log(z);
