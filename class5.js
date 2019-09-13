74da2d1f13252e1b27878c845fdf57b578c2f72e
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-09-13 11:41:29 -0400

    Add files via upload

---
+++ class5.js
@@ -0,0 +1,34 @@
+/* function whatDoYouDo (job, firstName){
+    switch(job){
+        case 'teacher':
+            return firstName + ' ' + 'teaches children';
+        case 'driver':
+            return firstName + ' ' + 'drives people.....over Bryan';
+        case 'designer':
+            return firstName + ' ' + 'deseigns things ';
+        default:
+            return firstName + ' ' + 'is broke and lazy';
+
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'Thami'));
+
+console.log(whatDoYouDo('driver', 'veda'));
+
+console.log(whatDoYouDo('musician', 'rob')); */
+
+//Initializing an array
+// elements start at 0 while length starts counting at 1
+
+let names = ['Brian', 'Andrew', 'Winny', 'Kaey'];
+let numbers = [1,25,6,7,8];
+let crazyArray = ['string', 203, true];
+/* console.log(names[0]);
+console.log(names.length);
+
+names.forEach(function(el){
+    console.log(el);
+}) */
+
+names.push('Melkadze');
